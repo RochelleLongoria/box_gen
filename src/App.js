6bb9b04dcfb95447a1f3e1cf08364b7dbf170b1e
Rochelle Longoria <rochellelong95@gmail.com>
2022-05-09 20:49:53 -0600

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React,{ useState } from 'react';
 import './App.css';
 
 function App() {
+  const [box, setBoxes] = useState([]) 
+
+  const[color, setcolor] = useState("")
+
+  const newBox = (e) =>{
+      e.preventDefault();
+    setBoxes([...box,color])
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <h1>Boxes</h1>
+          {JSON.stringify(box)}
+          <hr/>
+            <form onSubmit = {(e) => newBox(e) }>
+              <label>Pick a color</label>
+              <input type="text" onChange={(e) =>{setcolor(e.target.value)}}></input>
+              <button>Submit</button>
+            </form>
+          <div className='d-flex'>
+
+          {
+            box.map((color,index) =>{
+              return <div style={{backgroundColor: color, width:200, height: 200 , marginRight: 15}}></div>
+            })
+          }
+          </div>
+        </div>
+      )
 }
 
 export default App;
---
+++ src/components/boxes.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const box= (props) => {
+    return(
+        <div className="box">Box</div>
+    )
+}
+
+export default box
